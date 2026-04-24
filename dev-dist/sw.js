3a99d2049c9f3640c44f7002ea1882a0ab11ad6d
Kiran Wairagade <wairagadekiran7@gmail.com>
2026-04-24 17:31:36 +0530

    chore: update API endpoints and add service worker configuration

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hffoki7k5n4"
+    "revision": "0.i53hc7p57h"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
